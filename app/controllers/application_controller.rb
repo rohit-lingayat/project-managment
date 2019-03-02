b62ce6c405939896a620b8ca457c10bcfda20122
rohit-webonise <rohit.lingayat@weboniselab.com>
2019-03-02 18:35:45 +0530

    test1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # Catch all CanCan errors and alert the user of the exception
   rescue_from CanCan::AccessDenied do | exception|
-    redirect_to root_url, alert: exception.messages
+    redirect_to root_url, alert: exception.message
   end
 
   protected
